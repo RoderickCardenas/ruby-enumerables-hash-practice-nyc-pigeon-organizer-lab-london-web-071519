07e460ea9b147d024050506ba5fa767d033f2334
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 08:54:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -12,24 +12,14 @@ def nyc_pigeon_organizer(data)
         :lives => []
       }
       end
+      
     names.each do |name|
       data.each do |key, list|
         list.each do |info, nameS|
           if nameS.include?(name)
             newData[name][key] << info.to_s
-          binding.pry
         end
       end
     end
       newData
   end
-
-
-  #first collect all the names of the pigeons. 
-  #make those names the key to your new hash
-  #iterate over colors 
-
-# list.inject({}) do |r, h| 
-#   (r[h[:color]] ||= {}).merge!(h){ |key, old, new| old || new }
-#   r
-# end.values
