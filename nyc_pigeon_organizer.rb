5507563987e50f18d62224d340c1b82d23288d6d
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 08:51:47 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,6 +15,7 @@ def nyc_pigeon_organizer(data)
     names.each do |name|
       data.each do |key, list|
         list.each do |info, nameS|
+          if nameS.include?(name)
           binding.pry
         end
       end
