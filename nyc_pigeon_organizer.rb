d00399d69f080ee9eda6445043d7390ba0f33fdc
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 07:37:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newData = {}
+  data.each do |key, value|
+    #value.values pulls array of the names
+    binding.pry
+  end
 end
+
+# list.inject({}) do |r, h| 
+#   (r[h[:color]] ||= {}).merge!(h){ |key, old, new| old || new }
+#   r
+# end.values
