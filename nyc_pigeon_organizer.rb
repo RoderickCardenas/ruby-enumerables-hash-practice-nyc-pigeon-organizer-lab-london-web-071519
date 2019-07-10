54ab0a9f1dadb9f813110a05860be8307a0f0b39
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 08:18:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,13 +2,13 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   newData = {}
   
-  data.each do |key, (k, v)|
+  data.each do |key, (k, v)|    # binding.pry
     i = 0
     newData[values.values[0][i]] = {}
     i += 1
   end
     #value.values pulls array of the names
-    # binding.pry
+
   return newData
 end
 
