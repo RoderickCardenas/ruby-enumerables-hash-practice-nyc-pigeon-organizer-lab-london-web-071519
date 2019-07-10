c04a9148ce290a0630380058bfb009372290e0eb
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 08:41:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -18,8 +18,6 @@ def nyc_pigeon_organizer(data)
   #first collect all the names of the pigeons. 
   #make those names the key to your new hash
   #iterate over colors 
-  
-end
 
 # list.inject({}) do |r, h| 
 #   (r[h[:color]] ||= {}).merge!(h){ |key, old, new| old || new }
