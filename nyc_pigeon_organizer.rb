ca0a653ae04c734e2685e5148548f4cf996695fd
Roderick Cardenas <roderickcardenas1@gmail.com>
2019-07-10 07:57:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,48 +1,10 @@
 require 'pry'
 def nyc_pigeon_organizer(data)
-  # newData = {}
-  # data.each do |key, value|
-  #   #value.values pulls array of the names
-  #   binding.pry
-  
- newData = data.replace(
-      "Theo" => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Subway"]
-  },
-  "Peter Jr." => {
-    :color => ["purple", "grey"],
-    :gender => ["male"],
-    :lives => ["Library"]
-  },
-  "Lucky" => {
-    :color => ["purple"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  },
-  "Ms. K" => {
-    :color => ["grey", "white"],
-    :gender => ["female"],
-    :lives => ["Central Park"]
-  },
-  "Queenie" => {
-    :color => ["white", "brown"],
-    :gender => ["female"],
-    :lives => ["Subway"]
-  },
-  "Andrew" => {
-    :color => ["white"],
-    :gender => ["male"],
-    :lives => ["City Hall"]
-  },
-  "Alex" => {
-    :color => ["white", "brown"],
-    :gender => ["male"],
-    :lives => ["Central Park"]
-  }
-  )
-  return newData
+  newData = {}
+  data.each do |key, value|
+    #value.values pulls array of the names
+    binding.pry
+  end
 end
 
 # list.inject({}) do |r, h| 
